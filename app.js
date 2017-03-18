049674221d1019decc05449eb3a49b44b09277c8
rivermanvr <vrgithub@riversconsulting.com>
2017-03-18 16:26:49 -0400

    more setup of css and set up routes.js

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ app.engine('html', swig.renderFile);
 app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 app.use('/dist', express.static(path.join(__dirname, './dist')));
 app.use('/css', express.static(path.join(__dirname, './css')));
-
-app.get('/', (req, res, next) => {
-    res.render('index', { title: 'Home' })
-});
-
 app.use('/', routes);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const express = require( 'express' );
 const router = express.Router();
 
+app.get('/', (req, res, next) => {
+    res.render('index', { title: 'Home' })
+});
+
 module.exports = router;
