b92f94ee05a5049168752e5698b0abdea42b5f15
rivermanvr <vrgithub@riversconsulting.com>
2017-03-17 21:44:11 -0400

    built db/index.js file

--- db/index.js
+++ db/index.js
@@ -1,11 +1,20 @@
 const acmeDB = require( './db' );
+const User = require( './User' );
 
 const sync = () => {
     return acmeDB.sync({ force: true });
 };
 
-const seed = () => {
-    return sync()
+const addUsers = () => {
+    const promiseArr = [];
+    const user = ['Moe', 'Larry', 'Curly', 'Shep', 'Vince'];
+    for (let i = 0; i < user.length; i++) {
+        let name = user[i];
+        promiseArr.push(User.create({ name }));
+    }
+    return Promise.all(promiseArr);
 };
 
-module.exports = {seed, sync};
+const seed = () => sync().then(() => addUsers());
+
+module.exports = {models: { User }, seed, sync};
