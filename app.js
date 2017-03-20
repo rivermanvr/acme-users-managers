21bc794c747d99f947a117d1a19be18b0d46ddb7
rivermanvr <vrgithub@riversconsulting.com>
2017-03-20 19:28:18 -0400

    added body-parser, cleaned app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require( 'express' );
 const app = express();
-
-const swig = require( 'swig' );
-const path = require( 'path' );
 const routes = require( './routes' );
+const bodyParser = require( 'body-parser' );
+const path = require( 'path' );
 
-swig.setDefaults({ cache: false });
-app.set('view engine', 'html');
-app.engine('html', swig.renderFile);
+app.use(bodyParser.json());
 
 app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
