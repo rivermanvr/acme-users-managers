f4e9a5e932f84cfb4e57db7e3000fb64298a5176
rivermanvr <vrgithub@riversconsulting.com>
2017-03-20 15:41:54 -0400

    fixed static routes

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.set('view engine', 'html');
 app.engine('html', swig.renderFile);
 
 app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
-app.use('/dist', express.static(path.join(__dirname, './dist')));
-app.use('/css', express.static(path.join(__dirname, './css')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/', routes);
 
 module.exports = app;
