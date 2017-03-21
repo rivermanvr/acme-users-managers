d5e6a7c9a652c833eb02400164233ca27159acdc
rivermanvr <vrgithub@riversconsulting.com>
2017-03-20 21:01:06 -0400

    added swig back...

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require( 'express' );
 const app = express();
 const routes = require( './routes' );
+const swig = require( 'swig' );
 const bodyParser = require( 'body-parser' );
 const path = require( 'path' );
+
+swig.setDefaults({ cache: false });
+app.set( 'view engine', 'html' );
+app.engine( 'html', swig.renderFile );
 
 app.use(bodyParser.json());
 
